49247cd7abb9ec3504dcc871c4728f44d099c5a7
Przemek P <45264375+przemekplat@users.noreply.github.com>
2018-11-23 15:25:40 +0100

    Update primenumbers.rb
    - Optymalizacja
      * teraz skrypt zaczyna sprawdzanie podzielności od dwójki 
      * wystarczy że znajdzie tylko jedną podzielność (wcześniej musiał znaleźć aż trzy razem z jedynką i tą samą liczbą co było bez sensu)

--- primenumbers.rb
+++ primenumbers.rb
@@ -1,28 +1,23 @@
 def CzyLiczPierwsza(liczba) #funkcja sprawdza czy dana liczba jest liczbą pierwszą
-  i = 1.0
-  iloscDzielnikow = 0 #liczba dzielników
-  while i <= liczba 
-   if ((liczba / i) % 1).zero? then #sprawdzamy podzielność przez daną liczbę
-     iloscDzielnikow += 1
-     if iloscDzielnikow >= 3 then
-        break
+    i = 2.0
+    liczba
+    iloscDzielnikow = 0 #liczba dzielników
+    while i <= liczba
+     if ((liczba / i) % 1).zero? && i != liczba then #sprawdzamy podzielność przez daną liczbę
+        return true
+       end
+      i+=1
      end
-   end
-   i+=1
-  end
-  if iloscDzielnikow == 2 then #liczby pierwsze posiadają dokładnie dwa dzielniki naturalne: jedynkę i siebie samą
-    return true
-  else
-    return false
-  end
-end
+return false
+    end
+
 
 puts "Wprowadz zakres końcowy"
-usr_input = gets.chomp.to_i 
+usr_input = gets.chomp.to_i
 puts "Ze zbioru liczb od 0 do #{usr_input} liczbami pierwszymi są:"
 i = 0
 while i < usr_input
-  if CzyLiczPierwsza(i) == true
+  if CzyLiczPierwsza(i) == false
     puts i
   end
   i += 1
