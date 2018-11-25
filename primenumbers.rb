052eb01681a82f9edb50466dfee40328a31442e1
Przemek P <45264375+przemekplat@users.noreply.github.com>
2018-11-25 20:48:40 +0100

    Update primenumbers.rb
    optymalizacja

--- primenumbers.rb
+++ primenumbers.rb
@@ -1,11 +1,12 @@
 def CzyLiczPierwsza(liczba) #funkcja sprawdza czy dana liczba jest liczbą pierwszą
+if liczba > 10 then #liczby które mają na 
+   return false if liczba > 10 && (liczba % 10) == 2 || (liczba % 10) == 4 || (liczba % 10) == 5 || (liczba % 10) == 8 || (liczba % 10) == 0
+end
     i = 2.0
     while i <= liczba/2
-     if ((liczba / i) % 1).zero? && i != liczba then #sprawdzamy podzielność przez daną liczbę
-       return false
-       end
-      i+=1
-     end
+     return false if  ((liczba / i) % 1).zero?  #sprawdzamy podzielność przez daną liczbę
+     i+=1
+    end
 return true
     end
 
