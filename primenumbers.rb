ee7f73c5d8901988e39cda35a2942dcb385c4e1c
Przemek P <45264375+przemekplat@users.noreply.github.com>
2018-12-10 06:58:20 +0100

    Update primenumbers.rb
    Implementacja Sita Eratostenesa

--- primenumbers.rb
+++ primenumbers.rb
@@ -1,22 +1,21 @@
-def CzyLiczPierwsza(liczba) #funkcja sprawdza czy dana liczba jest liczbą pierwszą
-if liczba > 10 then #liczby które mają na 
-   return false if liczba > 10 && (liczba % 10) == 2 || (liczba % 10) == 4 || (liczba % 10) == 5 || (liczba % 10) == 8 || (liczba % 10) == 0
+def CzyLiczPierwsza(range_end) #funkcja sprawdza czy dana liczba jest liczbą pierwszą
+  array =  Array.new(range_end + 1, true)
+  array[0] = false
+  array[1] = false
+  i = 2
+  (2...Math.sqrt(range_end)).each do |i|
+    if array[i] == true
+      (2 * i).step(range_end, i) do |j|
+        array[j] = false
+      end
+    end
+  end
+  (0..array.length).each do |i|
+    puts i if array[i] == true
+  end
 end
-    i = 2.0
-    while i <= liczba/2
-     return false if  ((liczba / i) % 1).zero?  #sprawdzamy podzielność przez daną liczbę
-     i+=1
-    end
-return true
-    end
 
 puts "Wprowadz zakres końcowy"
-usrInput = gets.chomp.to_i
-puts "Ze zbioru liczb od 0 do #{usrInput} liczbami pierwszymi są:"
-i = 2
-while i <= usrInput
-  if CzyLiczPierwsza(i) == true
-    puts i
-  end
-  i += 1
-end
+range_end = gets.chomp.to_i
+puts "Ze zbioru liczb od 0 do #{range_end} liczbami pierwszymi są:"
+CzyLiczPierwsza(range_end)
